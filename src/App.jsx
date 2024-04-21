00e6d57c70a32eda8337a11e78fd5ea740314a02
Jesús David Díaz Hernández <jesus.diaz@ludycom.com>
2024-04-21 17:15:53 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+      </Router>
+    </>
+  );
+}
+
+export default App;
